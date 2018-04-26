755c02dc087faf0db5cdf2a517f36e440bc70558
ozgealgan <ozgealgn1@gmail.com>
2018-04-26 15:07:57 +0300

    initial commit

---
+++ Scrum/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scrum.Models;
+
+namespace Scrum.Controllers
+{
+	public class HomeController : Controller
+	{
+		ScrumDbEntities db = new ScrumDbEntities();
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
